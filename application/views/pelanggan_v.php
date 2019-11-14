123b273701e88c4d77a9b96ba4524a2f75516097
Risma Ayu <risma_ayu_26rpl@student.smktelkom-mlg.sch.id>
2019-11-14 21:21:33 +0700

    baru

--- application/views/pelanggan_v.php
+++ application/views/pelanggan_v.php
@@ -12,8 +12,8 @@
                                 <table class="table table-hover table-striped">
                                 <a href="#tambah" class="btn btn-primary" data-toggle="modal"><span class="glyphicon glyphicon-plus"></span> Tambah</a>                      
                                     <thead>
-                                        <th>NO</th>
-                                        <th>ID PELANGGAN</th>
+                                      <th>NO</th>
+                                      <th>ID PELANGGAN</th>
                                     	<th>NAMA LENGKAP</th>
                                     	<th>NOMOR HP</th>
                                     	<th>EMAIL</th>
