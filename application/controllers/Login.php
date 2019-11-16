7eedb079d1c054ec051635f1ad7594fbba1305e7
Mutiara Shaffana <mutiara_cinta_26rpl@student.smktelkom-mlg.sch.id>
2019-11-16 07:31:11 +0700

    baru

---
+++ application/controllers/Login.php
@@ -0,0 +1,44 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Login extends CI_Controller {
+	// public function __construct(){
+	// 	parent::__construct();
+	// 	$this->load->model('m_login');
+	// }
+
+	public function index()
+	{
+			$this->load->view('v_login');
+	}
+
+	public function login_cek(){
+		$this->load->model('m_login','login');
+		if($this->session->userdata('logged_in') == FALSE){
+
+			$this->form_validation->set_rules('username', 'username', 'trim|required');
+			$this->form_validation->set_rules('password', 'password', 'trim|required');
+
+			if ($this->form_validation->run() == TRUE) {
+				if($this->login->cek_user() == TRUE){
+					redirect('Dashboard','refresh');
+				} else {
+					$this->session->set_flashdata('notif', 'Login gagal');
+					redirect('Login','refresh');
+				}
+			} else {
+				$this->session->set_flashdata('notif', validation_errors());
+					redirect('Login','refresh');
+			}
+
+		} else {
+			redirect('Dashboard','refresh');
+		}
+	}
+
+	public function logout(){
+		$this->session->sess_destroy();
+		redirect('Login');
+	}
+
+}
---
+++ application/models/m_login.php
@@ -0,0 +1,30 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class m_login extends CI_Model {
+
+	public function cek_user()
+	{
+		$login_user = $this->db->join('level', 'level.id_level=admin.id_level')
+						->where('username', $this->input->post('username'))
+						->where('password', $this->input->post('password'))
+						->get('admin');
+	
+	if ($this->db->affected_rows()>0){
+		$data=$login_user->row();
+		$array = array('id_admin' => $data->id_admin,
+											'username' => $data->username,
+											'password' => $data->password,
+											'nama_level' => $data->nama_level,
+											'login_user' => true
+											);
+		$this->session->set_userdata($array);
+		return true;
+	}
+	else{
+		return false;
+	}
+  
+  }
+
+}
