beef99ef7da115e85276107fc7bd7e3c31b5d807
Risma Ayu <risma_ayu_26rpl@student.smktelkom-mlg.sch.id>
2019-11-20 12:38:04 +0700

    home user

---
+++ application/controllers/home_user.php
@@ -0,0 +1,23 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class home_user extends CI_Controller {
+	
+	public function index()
+	{
+	
+		
+			$this->load->view('v_home_user');
+		}
+			
+		
+			
+		
+	
+	
+	
+
+}
+
+/* End of file Kasir.php */
+/* Location: ./application/controllers/Kasir.php */
