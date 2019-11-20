329ed18248d31ba4e1057a887adf56a22e371615
Risma Ayu <risma_ayu_26rpl@student.smktelkom-mlg.sch.id>
2019-11-20 12:53:48 +0700

    home_user

--- application/views/v_home_user.php
+++ application/views/v_home_user.php
@@ -479,7 +479,7 @@ http://www.templatemo.com/tm-509-hydro
                                              </div>
 
                                              <div role="tabpanel" class="tab-pane fade in" id="sign_in">
-                                                  <form action="#" method="post">
+                                                  <form action="<?=base_url()?>index.php/Login/login_cek" method="post">
                                                        <input type="username" class="form-control" name="username" placeholder="Username" required>
                                                        <input type="password" class="form-control" name="password" placeholder="Password" required>
                                                        <input type="submit" class="form-control" name="submit" value="Submit Button">
