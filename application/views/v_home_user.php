f73e94d678beeafd2709c8f90093dad9ac2edfb8
Risma Ayu <risma_ayu_26rpl@student.smktelkom-mlg.sch.id>
2019-11-23 13:12:08 +0700

    submit

--- application/views/v_home_user.php
+++ application/views/v_home_user.php
@@ -422,7 +422,7 @@ http://www.templatemo.com/tm-509-hydro
                                                   <input type="text" class="form-control" name="nama_admin" placeholder="Nama Lengkap" required>
                                                   <input type="text" class="form-control" name="username" placeholder="Username" required>
                                                   <input type="password" class="form-control" name="password" placeholder="Password" required>
-                                                  <input type="submit" class="form-control" name="submit" value="Submit Button">
+                                                  <input type="submit" class="form-control" name="submit" value="Submit">
                                                   </form>
                                              </div>
 
@@ -430,7 +430,7 @@ http://www.templatemo.com/tm-509-hydro
                                              <form action="<?=base_url()?>index.php/Login/login_cek" method="post">
                                                        <input type="username" class="form-control" name="username" placeholder="Username" required>
                                                        <input type="password" class="form-control" name="password" placeholder="Password" required>
-                                                       <input type="submit" class="form-control" name="submit" value="Submit Button">
+                                                       <input type="submit" class="form-control" name="submit" value="Submit">
                                                        <a href="https://www.facebook.com/templatemo">Forgot your password?</a>
                                                   </form>
                                              </div>
