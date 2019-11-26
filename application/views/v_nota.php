c6a20b705de6f19dffbae848665639e066fbe76c
Risma Ayu <risma_ayu_26rpl@student.smktelkom-mlg.sch.id>
2019-11-26 21:03:22 +0700

    admin

--- application/views/v_nota.php
+++ application/views/v_nota.php
@@ -181,15 +181,9 @@
                                 Portfolio
                             </a>
                         </li>
-                        <li>
-                            <a href="#">
-                               Blog
-                            </a>
-                        </li>
+                       
                     </ul>
                 </nav>
-                <p class="copyright pull-right">
-                    &copy; <script>document.write(new Date().getFullYear())</script> <a href="http://www.creative-tim.com">Creative Tim</a>, made with love for a better web
-                </p>
+       
             </div>
         </footer>
